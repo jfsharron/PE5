38f6d131935e08878af56c202f438b4c11bf207a
Jonas Sharron 205 <jfsharron@msn.com>
2019-08-24 10:54:52 -0700

    added documentation header and main skeleton to source.c

--- ProjectEuler5/source.c
+++ ProjectEuler5/source.c
@@ -1,15 +1,24 @@
 //  ===========================================================================
-//  Program:              x
+//  Program:              ProjectEuler5
 //
-//  File:                 x
+//  File:                 source.c
 //
-//  Software Engineer:    x
+//  Software Engineer:    Jonas Sharron
 //
 //  Class:				  x
 //
-//  Date:                 x
+//  Date:                 24-August-2019
 //
-//  Purpose:              x
+//  Purpose:              The purpose of this program is to solve Project Euler
+//						  problem 5: 
+//
+//						  2520 is the smallest number that can be divided by 
+//                        each of the numbers from 1 to 10 without any remainder.
+//
+//                        What is the smallest positive number that is evenly
+//					      dividable by all the numbers from 1 to 20?
+//
+//                        https://projecteuler.net/problem=5
 //
 //  Parameters:           x
 //
@@ -19,4 +28,16 @@
 #include<stdio.h> 
 #include<string.h> 
 #include<stdlib.h> 
+
+
+//  ===========================================================================
+int main() {
+
+
+
+
+
+
+}  //  end main
+//  ===========================================================================
 
