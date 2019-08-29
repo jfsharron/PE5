ad6781ba056520c2225875471d109714e12e7aab
Jonas Sharron 205 <jfsharron@msn.com>
2019-08-29 10:35:51 -0700

    final commit -changed variable names to something more meaningful -changed do loop 1 to use answer instead of upperLimit -completed documentation

--- ProjectEuler5/source.c
+++ ProjectEuler5/source.c
@@ -5,78 +5,75 @@
 //
 //  Software Engineer:    Jonas Sharron
 //
-//  Class:				  x
-//
 //  Date:                 24-August-2019
 //
-//  Purpose:              The purpose of this program is to solve Project Euler
-//						  problem 5: 
-//
-//						  2520 is the smallest number that can be divided by 
+//	Purpose: 			  2520 is the smallest number that can be divided by 
 //                        each of the numbers from 1 to 10 without any remainder.
 //
 //                        What is the smallest positive number that is evenly
 //					      dividable by all the numbers from 1 to 20?
 //
-//                        https://projecteuler.net/problem=5
+//  Parameters:           no parameters passed, input given by user during 
+//                        execution
 //
-//  Parameters:           x
-//
-//  Returns:              x
+//  Returns:              Returns 0 for successful execution, answer output to
+//						  screen.
 //  ===========================================================================
 
 #include<stdio.h> 
 #include<string.h> 
 #include<stdlib.h> 
 
-
 //  ===========================================================================
 int main() {
 
-	int upperLimit = 0;
-	int flag = 0;
-	int i = 1;
+	int upperLimit = 0;		//  highest dividend (provided by user)
+	int flag = 0;			//  control flag for do loop 1
+	int flag2 = 0;			//  control flag for do loop 2
+	int dividend = 0;		//	value to divide by
+	int answer;				//  value starting at upperLimit and incremented
+							//  until all dividends mod 0
 
 	printf("Please enter the upper limit value: ");
 	scanf("%d", &upperLimit);
-	printf("\nVerify upperLimit: %d\n", upperLimit);  //  TODO remove
-	int answer = upperLimit;
 
+	answer = upperLimit;
 
-	//  do loop 1 to check values 1-upperLimit to see if upperLimit dividable
-	//  with no remainder
+	//  do loop 2 to repeat do loop 1 after incrementing answer variable 
+	//  (divisor)
 	do
 	{
-		if (upperLimit % i == 0)
+		flag = 0;
+		dividend = 1;
+		//  do loop 1 to check values 1-upperLimit to see if upperLimit dividable
+		//  with no remainder
+		do
 		{
-			flag = 0;
-			i++;
-			printf("flag = %d\n", flag);
-			printf("i = %d\n", i);		
-		}
-		else
-		{
-			flag = 1;
-			i++;
-			printf("flag = %d\n", flag);
-			printf("i = %d\n", i);
-		}
-	} while ((i <= upperLimit) && (flag == 0));
-	//  end do loop 1
+			if (answer % dividend == 0)
+			{
+				dividend++;
+			}
+			else
+			{
+				flag = 1;
+				answer++;
+			}
 
+			if (dividend == upperLimit)
+			{
+				flag2 = 1;
+			}
 
+		} while ((dividend <= answer) && (flag == 0));
+		//  end do loop 1
 
+	} while (flag2 == 0);
 
+	//  subtract one from answer to account for extra increment in above else
+	//  statement
+	answer = answer--;
 
-
-
-
-
-
-
-
-
-
+	printf("The smallest multiple is: %d\n", answer);
 
 }  //  end main
 //  ===========================================================================
